ba7e9230c98781d497db56b61cda300aed4a0615
Marius Fersigan <mariusfersigan@gmail.com>
2016-03-17 01:43:42 +0200

    Added comment.

--- FP13/Program.fs
+++ FP13/Program.fs
@@ -1,6 +1,6 @@
 ﻿open FunctionalTrie
 
-// running the demo - runTrie is an recursive function that calls itself with the new TreeMap obtained after you enter 
+// running the demo - runTrie is an recursive function that calls itself with the new TreeMap obtained after you enter some requests
 [<EntryPoint>]
 let main argv = 
     runTrie (Map([]))
